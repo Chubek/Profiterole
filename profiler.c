d6e0f569ba9904a2b9fe87eee252ffdb0ce8ba6d
Chubak Bidpaa <chubakbidpaa@gmail.com>
2023-07-01 04:05:18 +0430

    revised and refactorer

--- profiler.c
+++ profiler.c
@@ -1,42 +1,16 @@
-#define _POSIX_C_SOURCE 199309L
-#include <stdint.h>
-#include <signal.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <time.h>
-#include <mqueue.h>
-#include <semaphore.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 
-
-#ifndef ELAPSE_ID_SENTINEL
-#define ELAPSE_ID_SENTINEL 0xffffffff
-#endif
 
 #define BINFILE_MAGIC 0x74c787965cdbf970
-
-#define SIGNUM_REACHED_PROFPOINT SIGRTMIN
-#define SIGNUM_REACHED_SCRIPTEND RIGRTMIN + 1
 
 #define RETURN_SUCCESS 0
 #define RETURN_FAILURE -127
 
-#define TERM_REQUESTED(ELAPSEID) (ELAPSEID == ELAPSE_ID_SENTINEL)
-#define SEND_MESSAGE(CHANNEL, BUFFER) mq_send(CHANNEL, (char *)BUFFER, sizeof(elapsed_t), NULL)
-#define RECEIVE_MESSAGE(CHANNEL, BUFFER) mq_receive(CHANNEL, (char *)BUFFER, sizeof(elapsed_t), NULL)
+#define QWORD unsigned long
+#define DWORD int
 
-#define WAIT_FOR_SIGNALS(INFO, ...)                                     \
-  do {                                                                         \
-    int signal, signals[] = {__VA_ARGS__, 0}, *siptr = &signals[0];            \
-    sigset_t signalset;                                                        \
-    sigemptyset(&signalset);                                                   \
-    while ((signal = *siptr++))                                                \
-      sigaddset(&signalset, signal);                                           \
-    sigwaitinfo(&signalset, INFO);                                     \
-  } while (0)
+#define TERM_REQUESTED(FLAG) (FLAG < 0)
+#define SEND_MESSAGE(CHANNEL, BUFFER) mq_send(CHANNEL, (char *)BUFFER, sizeof(profinfo_t), NULL)
+#define RECEIVE_MESSAGE(CHANNEL, BUFFER) mq_receive(CHANNEL, (char *)BUFFER, sizeof(profinfo_t), NULL)
 
 #define WRITE_OUT_MAGIC(FD)                                                       \
   do {                                                                              \
@@ -44,22 +18,24 @@
     write(FD, &BINFILE_MAGIC_VAR, sizeof(unsigned long));            \
   } while (0)
 
-typedef unsigned long elpsid_t;
-typedef unsigned long elpsamnt_t;
-typedef int fld_t;
-typedef int yield_t;
+typedef QWORD markerid_t;
+typedef QWORD elapsed_t;
+typedef QWORD iflags_t;
+typedef DWORD fld_t;
+typedef DWORD yield_t;
 
 typedef struct ProfileIO {
-    mqd_t serialmsgfd;
+    mqd_t relegatemq;
     fld_t serialoutfd;
     pid_t pidserializer;
     pid_t pidprofiler;
 } profiler_t;
 
-typedef struct Elapse {
-    elpsid_t id;
-    elpsamnt_t amount;
-} elapsed_t;
+typedef struct ProfileInfo {
+    markerid_t id;
+    elapsed_t elapsed;
+    iflags_t flags;
+} profinfo_t;
 
 #if defined(__amd64__) && !defined(NO_ASM_FUNC)
 #define ELAPSE_AMOUNT_INSTRUCTION "rdtsc; movq %%rax, %0; shlq 32, %0; orq %%edx, %0;"
@@ -80,8 +56,8 @@ typedef struct Elapse {
 
 #ifndef NO_ASM_FUNC
 
-elpsamnt_t get_elapsed_amount() {
-    elpsamnt_t cputime;
+elapsed_t get_elapsed_since() {
+    elapsed_t cputime;
     asm volatile( ELAPSE_AMOUNT_INSTRUCTION 
         : "=r" (cputime)
         :
@@ -89,8 +65,8 @@ elpsamnt_t get_elapsed_amount() {
     return cputime;
 }
 
-elpsid_t get_elapsed_id(char *profname) {
-    elpsid_t qword;
+markerid_t byteptr_to_qword(char *profname) {
+    markerid_t qword;
     asm volatile( ELAPSE_ID_INSTRUCTION
             : "=r" (qword)
             : "r" (profname)
@@ -100,15 +76,15 @@ elpsid_t get_elapsed_id(char *profname) {
 
 #else
 
-elpsamnt_t get_elapsed_amount() {
+elapsed_t get_elapsed_since() {
     struct timespec ts;
     clock_gettime(CLOCK_REALTIME, &ts);
     return ts.tv_nsec;
 }
 
-elpsid_t get_elapsed_id(char *profname) {
-    elpsid_t qword = 0;
-    for (int i = 0; i < sizeof(elpsid_t); i++)
+markerid_t byteptr_to_qword(char *profname) {
+    markerid_t qword = 0;
+    for (int i = 0; i < sizeof(markerid_t); i++)
         qword |= profname[i] << (i << 3);
     return qword;
 }
@@ -116,47 +92,49 @@ elpsid_t get_elapsed_id(char *profname) {
 #endif
 
 
-void wait_for_messages_and_write(profiler_t *prof) {
-    ssize_t rcvlen;
-    elapsed_t elapsed;
+void poll_for_profile_and_serialize(profiler_t *prof) {
+    ssize_t rcvlen = 0;
+    profinfo_t pinfo = (profinfo_t) { 0 };
     WRITE_OUT_MAGIC(prof->serialoutfd);
     do {
-        if ((rcvlen = RECEIVE_MESSAGE(prof->serialmsgfd, &elapsed)) > 0)
-            write(prof->serialoutfd, &elapsed, rcvlen);
-    } while (!TERM_REQUESTED(elapsed.id));
+        if ((rcvlen = RECEIVE_MESSAGE(prof->relegatemq, &pinfo)) > 0)
+            write(prof->serialoutfd, &pinfo, rcvlen);
+    } while (!TERM_REQUESTED(pinfo.flag));
     close(prof->serialoutfd);
-    mq_close(prof->serialmsgfd);
+    mq_close(prof->relegatemq);
     exit(EXIT_SUCCESS);
 }
 
-void wait_for_signal_and_profile(profiler_t *prof) {
-    elapsed_t elapsed = (elapsed_t) { 0 };
-    siginfo_t signalinfo = (siginfo_t){ 0 };
-    elpsamnt_t currelapse = 0, lastelapse = 0;
+void poll_for_info_and_profile(profiler_t *prof) {
+    ssize_t rcvlen = 0;
+    profinfo_t pinfo = (profinfo_t) { 0 };
+    elapsed_t currelapse = 0, lastelapse = 0;
     do {
-        WAIT_FOR_SIGNALS(&signalinfo, SIGNUM_REACHED_PROFPOINT);
-        currelapse = get_elapsed_amount();
-        elapsed.id = signalinfo.si_value.sival_int;
-        elapsed.amount = currelapse - lastelapse;
-        lastelapse = currelapse;
-        SEND_MESSAGE(prof->serialmsgfd, &elapsed);
-    } while (!TERM_REQUESTED(elapsed.id));
+        if ((rcvlen = RECEIVE_MESSAGE(prof->relegatemq, &pinfo)) > 0) {
+            currelapse = get_elapsed_since();
+            pinfo.id = signalinfo.si_value.sival_int;
+            pinfo.elapsed = currelapse - lastelapse;
+            lastelapse = currelapse;
+            SEND_MESSAGE(prof->relegatemq, &pinfo);
+        }
+    } while (!TERM_REQUESTED(pinfo.flag));
     close(prof->serialoutfd);
-    mq_close(prof->serialmsgfd);
+    mq_close(prof->relegatemq);
     exit(EXIT_SUCCESS);
 }
 
-yield_t send_signal_to_profiler(profiler_t *prof, char *pointid) {
-    elpsid_t elapseid = get_elapsed_id(pointid);
-    return sigqueue(prof->pidprofiler, SIGNUM_REACHED_PROFPOINT, (union sigval){ .sival_int = elapseid });
+yield_t queue_message_to_profiler(profiler_t *prof, char *markerid, iflags_t flags) {
+    markerid_t qwdid = byteptr_to_qword(markerid);
+    profinfo_t pinfo = (profinfo_t){ .id = qwdid, .flags = flags };
+    return SEND_MESSAGE(prof->relegatemq, &pinfo);
 }
 
 yield_t init_profiler(profiler_t *prof, char *profname, char *outpath) {
     yield_t yield;
     pid_t cpid1, cpid2;
-    if ((yield = prof->serialmsgfd = mq_open(profname, O_CREAT)) < 0)
+    if ((yield = prof->relegatemq = mq_open(profname, O_CREAT | O_RDWR)) < 0)
         return yield;
-    else if ((yield = prof->serialoutfd = open(outpath, O_CREAT)) < 0)
+    else if ((yield = prof->serialoutfd = open(outpath, O_CREAT | O_RDWR)) < 0)
         return yield;
     else if ((yield  = prof->pidserializer = cpid1 = fork()) < 0)
         return yield;
@@ -165,11 +143,11 @@ yield_t init_profiler(profiler_t *prof, char *profname, char *outpath) {
     
     if (!cpid1 || !cpid2) {
         if (!cpid1) {
-            wait_for_messages_and_write(prof);
+            poll_for_profile_and_serialize(prof);
         }
 
         if (!cpid2) {
-            wait_for_signal_and_profile(prof);
+            poll_for_info_and_profile(prof);
         }    
     } else {
         return RETURN_SUCCESS;
