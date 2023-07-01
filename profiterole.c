8f075807281458a05fc0eff1f648439f2e8fc2c5
Chubak Bidpaa <chubakbidpaa@gmail.com>
2023-07-01 18:25:05 +0430

    added dealloc, changes in names, removed ifndefs

--- profiterole.c
+++ profiterole.c
@@ -3,45 +3,16 @@
 #include <stdlib.h>
 #include "profiler.h"
 
-#ifndef UQWORD
 #define UQWORD unsigned long
-#endif
-
-#ifndef UDWORD
 #define UDWORD unsigned int
-#endif
-
-#ifndef UWORD
 #define UWORD  unsigned short
-#endif
-
-#ifndef UBYTE
 #define UBYTE unsigned char
-#endif
-
-#ifndef IQWORD
 #define IQWORD long
-#endif
-
-#ifndef IDWORD
 #define IDWORD int
-#endif
-
-#ifndef IWORD
 #define UWORD  short
-#endif
-
-#ifndef IBYTE
 #define IBYTE char
-#endif
-
-#ifndef MEMADR
-#define MEMADR void *;
-#endif
-
-#ifndef NOTYPE
-#define NOTYPE void
-#endif
+#define MEMADR void*
+#define NORETURN void
 
 #ifndef NAME_KWARG
 #define NAME_KWARG "name"
@@ -55,9 +26,9 @@
 #define BREAKID_KWARG "breakid"
 #endif
 
-#define NIL
+#define STRARG const char
 
-#define STRARG_TYY const char
+#define NIL
 
 #define EXCEPT PyObject *
 #define EXTOBJ ProfiteroleObj
@@ -70,6 +41,7 @@
 #define FUNCNAME_NEWFN create
 #define FUNCNAME_INITFN initialize
 #define FUNCNAME_CALLFN callproto
+#define FUNCNAME_DEALLOCFN freeobjmem
 
 #define PYTYY_DEF(NAME) PyTypeObject *NAME
 #define PYOBJ_DEF(NAME) PyObject *NAME
@@ -78,13 +50,22 @@
 
 #define DECL_FUNCNAME(RETTYPE, FUNCNAME, FIRST_ARG) 
 	static RETTYPE FUNCNAME_PREFIX ## FUNCNAME (FIRST_ARG, PYOBJ_DEF(args), PYOBJ_DEF(kwargs))
+
+#define DECL_STATICOBJ(OBJTYY, OBJNAME)\
+	static OBJTYY OBJNAME =
+
 #define GET_FUNCNAME(FUNCNAME) FUNC_PREFX ## FUNCNAME
-#define KWARGS_NAMES(...) static char *kwargslist = {  __VA_ARGS__, NULL  }
+
+#define KWARGS_NAMES(...) \
+	static char *kwargslist = {  __VA_ARGS__ , NULL  }
+
 #define PARSE_ARGS(FMT, ...)	\
 	if (!(PyArg_ParseTupleAndKeywords(args, kwargs, FMT, kwargslist, __VA_ARGS__)))	\
 		return PyErr_BadArgument()
-#define NEW_EXCEPTION(NAME, NS) 
+
+#define NEW_EXCEPTION(NAME, NS) \
 	EXCEPT NAME = PyErr_NewException(NS, NULL, NULL)
+
 
 typedef MEMADR PTROBJ;
 
@@ -92,6 +73,7 @@ typedef struct {
 	PyObject_HEAD
 	PYOBJ_DEF(profptr);
 } EXTOBJ;
+
 
 DECL_FUNCNAME(PYOBJ_DEF(NIL), FUNCNAME_NEWFN, PYTYY_DEF(type)) {
 	PTROBJ profptr;
@@ -106,7 +88,7 @@ DECL_FUNCNAME(PYOBJ_DEF(NIL), FUNCNAME_NEWFN, PYTYY_DEF(type)) {
 DECL_FUNCNAME(CCTYY_DEF(IDWORD, NIL), FUNCNAME_INITFN, CCPTR_DEF(EXTOBJ, self)) {
 	NEW_EXCEPTION(profxcpt, XCPTNS);
 	KWARGS_NAMES(NAME_KWARG, OUTPATH_KWARG);
-	STRARG_TYY *name = NULL, *outpath = NULL;
+	STRARG *name = NULL, *outpath = NULL;
 	PARSE_ARGS("ss", name, outpath);
 	PTROBJ profptr = PyLong_AsVoidPtr(self->profptr);
 	if ((init_profiler((PRFREF)profptr, name, outpath)) < 0)
@@ -117,12 +99,18 @@ DECL_FUNCNAME(CCTYY_DEF(IDWORD, NIL), FUNCNAME_INITFN, CCPTR_DEF(EXTOBJ, self)) 
 DECL_FUNCNAME(PYOBJ_DEF(NIL), FUNCNAME_CALLFN, CCPTR_DEF(EXTOBJ, self)) {
 	NEW_EXCEPTION(profxcpt, XCPTNS);
 	KWARGS_NAMES(BREAKID_KWARG);
-	STRARG_TYY *breakid = NULL;
+	STRARG *breakid = NULL;
 	PARSE_ARGS("s", breakid);
 	PTROBJ profptr = PyLong_AsVoidPtr(self->profptr);
-	if ((queue_message_to_profiler((PROFCC_REFR)profptr, breakid)) == RETURN_FAILURE)
+	if (!(queue_message_to_profiler((PROFCC_REFR)profptr, breakid)))
 		return PyErr_SetFromErrno(profxcpt);
 	return NULL;
+}
+
+DECL_FUNCNAME(NORETURN, FUNCNAME_DEALLOCFN, CCPTR_DEF(EXTOBJ, self)) {
+	PTROBJ profptr = PyLong_AsVoidPtr(self->profptr);
+	PyMem_RawFree(profptr);
+	Py_TYPE(self)->tp_free(PYOBJ_DEF(NIL)self);
 }
 
 static PyTpeObject ProfiteroleProfiler = {
