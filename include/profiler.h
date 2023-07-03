3eca2897afafec096c946eea3cb391aaad0719e2
Chubak Bidpaa <chubakbidpaa@gmail.com>
2023-07-03 17:52:10 +0430

    fixed includes

--- include/profiler.h
+++ include/profiler.h
@@ -5,17 +5,11 @@
 #define _POSIX_C_SOURCE 199309L
 #endif
 
-#include <fcntl.h>
-#include <mqueue.h>
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/times.h>
-#include <unistd.h>
-#include "roulettemacs.h"
-
 #define QWORD_MAX 0xffffffffffffffff
+
+#define QWORD unsigned long
+#define DWORD int
+#define UDWORD unsigned int
 
 #define RETURN_SUCCESS 1
 #define RETURN_FAILURE 0
@@ -27,7 +21,7 @@
 #define TERM_REQUESTED (pinfo.id == prof->sentinel)
 
 #if defined(__amd64__) && !defined(NO_ASM_FUNC)
-#define ASM_ELAPSE "rdtsc; movq %%rax, %0; shlq 32, %0; orq %%edx, %0;"
+#define ASM_ELAPSE "rdtsc; movq %%rax, %0; shlq $32, %0; orq %%rdx, %0;"
 #define ASM_BUF2QW "movq (%1), %%r11; movq %%r11, %0"
 #define ASM_QW2BUF "movq %1, (%0);"
 #define CLB_ELAPSE "rax", "rdx"
@@ -58,13 +52,13 @@
 #define MEMCOPYFN CUSTOM_MEMCOPYFN
 #endif
 
-typedef UNSIGNED_Quad sentinel_t;
-typedef UNSIGNED_Quad markerid_t;
-typedef UNSIGNED_Quad elapsed_t;
-typedef UNSIGNED_Quad snl_t;
-typedef UNSIGNED_Double fld_t;
-typedef UNSIGNED_Double yield_t;
-typedef SIGNIFY_None nonyield_t;
+typedef QWORD sentinel_t;
+typedef QWORD markerid_t;
+typedef QWORD elapsed_t;
+typedef QWORD snl_t;
+typedef DWORD fld_t;
+typedef DWORD yield_t;
+typedef void nonyield_t;
 
 typedef struct Profiler {
   mqd_t relegatemq;
@@ -79,14 +73,31 @@ typedef struct ProfileInfo {
   elapsed_t elapsed;
 } profinfo_t;
 
-elapsed_t get_elapsed_since(NILTY);
-markerid_t buffer_to_qword(char const *src);
-void qword_to_buffer(char const *dst, const QWORD src);
+elapsed_t get_elapsed_since(void);
+markerid_t buffer_to_qword(const char *src);
+nonyield_t qword_to_buffer(char *dst, const QWORD src);
+
+nonyield_t hash_sentinel(const char *profname, QWORD *sentinelqword, char *sentinelbytes);
 
 nonyield_t poll_for_profile_and_serialize(profiler_t *prof);
 nonyield_t poll_for_info_and_profile(profiler_t *prof);
 
-yield_t init_profiler(profiler_t *prof, const char *profname, const char *outpath, const char * sentinelbytes);
+yield_t init_profiler(profiler_t *prof, const char *profname, const char *outpath, char * sentinelbytes);
 yield_t queue_message_to_profiler(profiler_t *prof, const char *markerid);
+
+#define SEND_MESSAGE(CHANNEL, BUFFER)                                          \
+  mq_send(CHANNEL, (char *)BUFFER, sizeof(profinfo_t), priority)
+#define RECEIVE_MESSAGE(CHANNEL, BUFFER)                                       \
+  mq_receive(CHANNEL, (char *)BUFFER, sizeof(profinfo_t), &priority)
+#define WRITE_BINMAGIC(FD)                                                      \
+  do {                                                                         \
+    QWORD sntnl = BINFILE_SENTINEL;                                            \
+    write(FD, &sntnl, sizeof(QWORD));                                          \
+  } while (0)
+#define YIELD_IF_ERR(...)                                                      \
+  if ((yield = __VA_ARGS__) < 0)                                               \
+  return yield
+
+
 
 #endif
