5b4995ef5e522503f280371db3cc5366678d6f4b
Chubak Bidpaa <chubakbidpaa@gmail.com>
2023-07-05 04:03:47 +0430

    QOL script for autotools

---
+++ autogen.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [[ $1 == -re ]]; then
+	autoreconf
+	exit;
+elif [[ $1 == -rm ]]; then
+	rm -f aclocal.m4
+	rm -f config.guess
+	rm -f config.sub
+	rm -f configure
+	rm -f depcomp
+	rm -f ltmain.sh
+	rm -f configure~
+	rm -f configure.ac~
+	rm -rf autom4te.cache
+	rm -rf build
+	rm -rf pyprofiterole.egg-info
+	exit;
+fi
+
+libtoolize
+aclocal
+automake
+autoconf
