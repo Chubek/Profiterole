9db449463ac620598e91e98729b9be416a5f9c41
Chubak Bidpaa <chubakbidpaa@gmail.com>
2023-07-05 06:24:29 +0430

    autotools scripts generated

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,12 @@
-#!/bin/bash
+#!/bin/bash -x
 
 if [[ $1 == -re ]]; then
-	autoreconf
+	shift
+	mkdir -p m4
+	exec autoreconf -fi $@
 	exit;
 elif [[ $1 == -rm ]]; then
+	shift
 	rm -f aclocal.m4
 	rm -f config.guess
 	rm -f config.sub
@@ -13,15 +16,23 @@ elif [[ $1 == -rm ]]; then
 	rm -f configure~
 	rm -f configure.ac~
 	rm -f profiterole/config.h.in
+	rm -f missing
+	rm -f compile
+	rm -f install-sh
 	rm -rf autom4te.cache
 	rm -rf build
 	rm -rf pyprofiterole.egg-info
 	rm -rf m4
+	rm -rf $@
+	exit;
+elif [[ $1 == -rx ]]; then
+	shift
+	libtoolize
+	aclocal
+	autoheader
+	autoconf
+	automake --add-missing
+	$(@)
 	exit;
 fi
 
-autoheader
-libtoolize
-aclocal
-automake
-autoconf
